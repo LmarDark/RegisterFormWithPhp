657275be78b38abd29af380955c7945b6f31152a
Lucas Matheus <105503258+LmarDark@users.noreply.github.com>
2024-02-28 23:05:01 -0400

    Update env.php

--- env.php
+++ env.php
@@ -2,10 +2,10 @@
 session_start();
 class Env{
 
-    protected string $servername = "XXXXXXXXX";
-    protected string $user = "XXXXXXXXX";
-    protected string $password = "XXXXXXXXX";
-    protected string $db = "XXXXXXXXX";
+    protected string $servername = "XXXXXXXXXXXX";
+    protected string $user = "XXXXXXXXXXXX";
+    protected string $password = "XXXXXXXXXXXX";
+    protected string $db = "XXXXXXXXXXXX";
     protected $conn;
 
     public function ConnectionDB(){
@@ -23,11 +23,42 @@ class Env{
                         $register_sql = "INSERT INTO test(user, password) VALUES('$user_php', '$hash_pass')";
                         
                         if(mysqli_query($conn, $register_sql)){
-                            echo "Dados cadastrados com sucesso!";
+                            echo "<script>alert('Dados cadastrados com sucesso!')</script>";
                         } else {
-                            echo "Erro ao cadastrar usuário!";
+                            echo "<script>alert('Erro ao cadastrar usuário!')</script>";
                         };
 
+                    }
+                }
+            }
+        }
+    }
+    public function log_function(){
+        if($_SERVER['REQUEST_METHOD'] === "POST"){
+            $conn = mysqli_connect($this->servername, $this->user, $this->password, $this->db);
+            if ($conn) {
+                if(isset($_POST["user_log"]) && ($_POST["password_log"])){
+                    $log_user = $_POST["user_log"];
+                    $log_password = $_POST["password_log"];
+
+                    $log_sql = sprintf("SELECT * FROM test WHERE user = '%s'",
+                    $conn->real_escape_string($_POST["user_log"]));
+
+                    $result = $conn->query($log_sql);
+                    $user = $result->fetch_assoc();
+                    
+                    if($user === null){
+                        echo "<script>alert('Não encontrado nenhum usuário com este nome!')</script>";
+                    } else {
+                        if($user){
+                            if (password_verify($_POST["password_log"], $user["password"])) {
+                                $_SESSION["username"] = "$log_user";
+                                echo "<script>alert('Login feito com sucesso!')</script>";
+                                header("Location: home.php");
+                            } else {
+                                echo "<script>alert('Usuário ou senha incorretos!')</script>";
+                            }
+                        }
                     }
                 }
             }
